a5b17fb3d739cc3cf4ff17393dc7abda34e87f19
Rostyslav Klym <rostik.klym@gmail.com>
2023-04-09 21:28:31 +0300

    refactored

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
+function getEpisodesData(url) {
+  return fetch(url)
+    .then((response) => response.json())
+    .then((data) => data);
+}
+
 function renderEpisodesList(episodesList, episodesContainer) {
+  episodesContainer.classList.remove("loader");
+
   const ul = document.createElement("ul");
   ul.classList.add("episodes-list");
 
-  const episodesCharacters = []; // Using for throwing the characters links array
-
   const listItems = episodesList
     .sort(({ episodeId: a }, { episodeId: b }) => a - b)
-    .map(({ episodeId, name, openingCrawl, characters }) => {
-      episodesCharacters.push({ episodeId, characters });
-
+    .map(({ episodeId, name, openingCrawl }) => {
       const li = document.createElement("li");
 
       const header = document.createElement("h2");
@@ -25,18 +29,16 @@ function renderEpisodesList(episodesList, episodesContainer) {
 
   ul.append(...listItems);
   episodesContainer.append(ul);
-
-  return episodesCharacters;
 }
 
-function renderEpisodeCharacters({ episodeId, characters: charactersLinks }) {
+function renderEpisodeCharacters({ episodeId, characters }) {
   const episodeLi = document.querySelector(`.episode-${episodeId}`);
   const episodeCharacters = document.createElement("p");
   episodeCharacters.classList.add("loader");
   episodeLi.append(episodeCharacters);
 
   Promise.all(
-    charactersLinks.map((url) =>
+    characters.map((url) =>
       fetch(url)
         .then((response) => response.json())
         .then((data) => data.name)
@@ -47,12 +49,26 @@ function renderEpisodeCharacters({ episodeId, characters: charactersLinks }) {
   });
 }
 
+function errorHandler(error, element) {
+  element.classList.remove("loader");
+  element.innerText = error.message;
+}
+
 const loadBtn = document.querySelector("#load-btn");
-const episodesSection = document.querySelector(".episodes");
 
 loadBtn.addEventListener("click", (event) => {
-  fetch("https://ajax.test-danit.com/api/swapi/films")
-    .then((response) => response.json())
-    .then((data) => renderEpisodesList(data, episodesSection))
-    .then((characters) => characters.forEach(renderEpisodeCharacters));
+  loadBtn.disabled = true;
+  const episodesSection = document.querySelector(".episodes");
+  episodesSection.classList.add("loader");
+
+  getEpisodesData("https://ajax.test-danit.com/api/swapi/films")
+    .then((data) => {
+      renderEpisodesList(data, episodesSection);
+      return data;
+    })
+    .then((data) => data.forEach(renderEpisodeCharacters))
+    .catch((e) => {
+      errorHandler(e, episodesSection);
+      loadBtn.disabled = false;
+    });
 });
