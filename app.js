1f762cd3ebe75bdfc9e4725091c5f1d36ec344d7
Rostyslav Klym <rostik.klym@gmail.com>
2023-04-09 18:57:32 +0300

    added fetch for characters

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
-const loadBtn = document.querySelector("#load-btn");
-const episodesSection = document.querySelector(".episodes");
-
-loadBtn.addEventListener("click", (event) => {
-  fetch("https://ajax.test-danit.com/api/swapi/films")
-    .then((response) => response.json())
-    .then((data) => {
-      renderEpisodesList(data, episodesSection);
-      console.log(data);
-    });
-});
-
 function renderEpisodesList(episodesList, episodesContainer) {
   const ul = document.createElement("ul");
+  ul.classList.add("episodes-list");
+
+  const episodesCharacters = []; // Using for throwing the characters array
 
   const listItems = episodesList
     .sort(({ episodeId: a }, { episodeId: b }) => a - b)
-    .map(({ episodeId, name, openingCrawl }) => {
+    .map(({ episodeId, name, openingCrawl, characters }) => {
+      episodesCharacters.push({ episodeId, characters });
+
       const li = document.createElement("li");
 
       const header = document.createElement("h2");
@@ -24,6 +17,7 @@ function renderEpisodesList(episodesList, episodesContainer) {
       const description = document.createElement("p");
       description.innerText = openingCrawl;
 
+      li.classList.add(`episode-${episodeId}`);
       li.append(header, description);
 
       return li;
@@ -31,4 +25,29 @@ function renderEpisodesList(episodesList, episodesContainer) {
 
   ul.append(...listItems);
   episodesContainer.append(ul);
+
+  return episodesCharacters;
 }
+
+function renderEpisodeCharacters({ episodeId, characters }) {
+  const episodeLi = document.querySelector(`.episode-${episodeId}`);
+  const episodeCharacters = document.createElement("p");
+
+  characters.forEach((url) =>
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => episodeCharacters.append(data.name + ", "))
+  );
+
+  episodeLi.append(episodeCharacters);
+}
+
+const loadBtn = document.querySelector("#load-btn");
+const episodesSection = document.querySelector(".episodes");
+
+loadBtn.addEventListener("click", (event) => {
+  fetch("https://ajax.test-danit.com/api/swapi/films")
+    .then((response) => response.json())
+    .then((data) => renderEpisodesList(data, episodesSection))
+    .then((characters) => characters.forEach(renderEpisodeCharacters));
+});
